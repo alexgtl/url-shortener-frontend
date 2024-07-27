fc24c6e88b795f19461c9c392d678674cab415b2
Alex Gutierrez <agutierrezlavin@gmail.com>
2024-07-27 13:47:00 +0200

    WIP: Update UI

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,13 +1,12 @@
 <template>
-  <main class="min-h-screen w-full flex justify-center">
-    <div class="max-w-[1540px]">
-      <section class="w-full min-h-screen pt-[30dvh] lg:px-[10dvw]">
-        <div class="px-[2dvw] max-w-[1920px] h-full items-center w-full relative title">
-          <h1 class="mb-10 text-4xl font-bold leading-tight pl-4 md:pl-0 text-center w-full">
+  <main class="w-full flex justify-center">
+    <div class="max-w-[1440px]">
+      <section class="pt-[30dvh]">
+        <div class="">
+          <h1 class="mb-10 text-4xl leading-tight pl-4 md:pl-0 text-left w-full">
             Transform your URL into an <span class="text-accent italic">agutierrez.link</span>
             shorted url here.
           </h1>
-
           <UrlShortenerCard />
         </div>
       </section>
